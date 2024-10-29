1ba8484b079e58fd906b3285dc26eb60d86c2814
sans18-coder <iksanwijaya018@gmail.com>
2024-10-29 15:33:29 +0700

    component Button,FromLogin

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+/* 
+- background(buat ganti background button),
+- variant(nambahin style tambahan yang di butuhin buat buttonnya style bawaan yang ku setting cuma rounded,ukuran text,padding,margin,backgoundnya doang),
+- children(semua yang ada di dalem tag Buttonnya nanti)
+
+contoh gunain nantinya gini : 
+<Button background='red' variant='text-white blablabla' >yang di dalem ini childrennya bebas mau tag apa aja atau bacaan apa aja mau pake span juga buat font awesome boleh</Button>
+*/
+
+const Button = (props) => {
+  const { children, variant, background, type = "button" } = props;
+  return (
+    <button
+      type={`${type}`}
+      class={`${variant} bg-${background}-700 hover:bg-${background}-800 focus:ring-4 focus:ring-${background}-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:${background}-600 dark:hover:bg-${background}-700 focus:outline-none dark:focus:ring-${background}-800`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
