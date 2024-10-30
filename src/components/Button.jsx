bbb9dd4d0ebf2eee508ceb50eed74e2272f34328
sans18-coder <iksanwijaya018@gmail.com>
2024-10-30 23:37:49 +0700

    add component header,botton,inputfield,formLogin

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../assets/style/index.css";
 /* 
 - background(buat ganti background button),
 - variant(nambahin style tambahan yang di butuhin buat buttonnya style bawaan yang ku setting cuma rounded,ukuran text,padding,margin,backgoundnya doang),
@@ -10,11 +10,16 @@ contoh gunain nantinya gini :
 */
 
 const Button = (props) => {
-  const { children, variant, background, type = "button" } = props;
+  const {
+    children,
+    variant = "text-white",
+    background = "blue",
+    type = "button",
+  } = props;
   return (
     <button
       type={`${type}`}
-      class={`${variant} bg-${background}-700 hover:bg-${background}-800 focus:ring-4 focus:ring-${background}-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:${background}-600 dark:hover:bg-${background}-700 focus:outline-none dark:focus:ring-${background}-800`}
+      className={`${variant} bg-${background}-700 hover:bg-${background}-800 focus:ring-4 focus:ring-${background}-300 font-medium rounded-lg text-sm px-4 py-2 me-2 mb-2 dark:${background}-600 dark:hover:bg-${background}-700 focus:outline-none dark:focus:ring-${background}-800`}
     >
       {children}
     </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import "../assets/style/index.css";
+import Button from "../components/button";
+import FormLogin from "../components/FormLogin";
+import InputField from "../components/InputField";
+import Header from "../components/Header";
 function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div>
+      <Header></Header>
+      <InputField name="nama" id="nama">
+        Nama
+      </InputField>
+      <FormLogin method="POST" action="/login"></FormLogin>
+      <Button>Ucup</Button>
+    </div>
+  );
 }
 
 export default Home;
